7b335384de46ce5979c7c2072dcf96f0463cf681
hyun <h.zoo3963>
2023-06-29 09:08:45 +0900

    init commit

---
+++ src/main/java/com/sparta/springweek1test/SpringWeek1TestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springweek1test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek1TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeek1TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springweek1test/SpringWeek1TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springweek1test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeek1TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
